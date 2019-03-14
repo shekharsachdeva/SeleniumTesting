f40e709196f11b3211d4c76fb02eef50c505e9ac
shekhar.sachdeva <shekhar.sachdeva@DDC2-DX-6V9R9B2.dir.svc.accenture.com>
2019-03-14 12:51:06 +0530

    fifth

--- Test/src/testing/Class20.java
+++ Test/src/testing/Class20.java
@@ -2,7 +2,7 @@ package testing;
 
 public class Class20 {
 	
-	public void display2() {
+	public void display3() {
 		
 		System.out.println("hello display method");
 		
---
+++ Test/src/testing/Class21.java
@@ -0,0 +1,10 @@
+package testing;
+
+public class Class21 {
+
+	
+	public Class21() {
+	System.out.println("this is counstructor");
+	}
+
+}
